2e8f88b111a06a5281a8c80c79c7f0581b6cf0b3
PaulCondori <pscondoric@unjbg.edu.pe>
2025-06-24 21:04:47 -0500

    Ejercicio terminado: Programa funcional

--- ejemplo1.cpp
+++ ejemplo1.cpp
@@ -16,9 +16,40 @@ int main(){
         cin>>EMPLEADOS[i].num;
         cin.ignore();
         getline (cin, EMPLEADOS[i].nom);
-        for (int j=0; j<12;j++)
+        for (int j=0; j<12;j++){
         cin>>EMPLEADOS[i].ven[j];
+        }
         cin>>EMPLEADOS[i].sal;
+    }
+    int maxIndex = 0;
+    float maxVentas = 0;
+    for (int i = 0; i < n; i++){
+        float totalVentas = 0;
+        for (int j = 0; j < 12; j++){
+            totalVentas += EMPLEADOS[i].ven[j];
+        }
+        if (totalVentas > maxVentas){
+            maxVentas = totalVentas;
+            maxIndex = i;
+        }
+    }
+    cout << " Empleado con mayores ventas anuales: " << endl;
+    cout << "Numero: " << EMPLEADOS[maxIndex].num << endl;
+    cout << "Nombre: " << EMPLEADOS[maxIndex].nom << endl;
+    for (int i = 0; i < n; i++){
+        float total = 0;
+        for (int j = 0; j < 12; j++){
+            total += EMPLEADOS[i].ven[j];
+        }
+        if (total > 100){
+            EMPLEADOS[i].sal *= 1.10;
+        }
+    }
+    cout << " Empleados con menos de 30 ventas en diciembre: ";
+    for (int i = 0; i < n; i++){
+        if (EMPLEADOS[i].ven[11] < 30){
+            cout << "Numero: " <<EMPLEADOS[i].num << ", Nombre: " << EMPLEADOS[i].nom << endl;
+        }
     }
     return 0;
 }
